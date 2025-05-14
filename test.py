7bab5e38327585b93d15d22c348704640fce61fa
Cyriltrn <traineaucyril8@gmail.com>
2025-05-14 16:10:26 -0400

    test

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ print(somme(1,1))
 def aire_rectangle(h, b):
     return h*b
 
-print("Test de push")
-
 def dire_bonjour(message, nom):
     return message + nom
 
 def question():
     input("Ton ordi est casse?")
+
+print("Test de push")
