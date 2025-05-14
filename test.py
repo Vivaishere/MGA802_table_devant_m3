6cc87a9ccdb51dc0b18d03a9e3f171199d0538ed
Cyriltrn <traineaucyril8@gmail.com>
2025-05-14 16:05:46 -0400

    test

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-# C'est pour les testes
+# C'est pour les tests
 def somme(a,b):
     return a+b
 
 print(somme(1,1))
+
+print("Test de push")
 
 def dire_bonjour(message, nom):
     return message + nom
